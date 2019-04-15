b17648909bf1349b17f250aa851296bb725d90f9
Kris <christttto@berkeley.edu>
2019-04-15 00:38:03 -0700

    render pole grid somehow

--- src/App.js
+++ src/App.js
@@ -1,28 +1,41 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react"
+import "./App.css"
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      pole1: [3, 2, 1],
+      pole2: Array(3).fill(null),
+      pole3: Array(3).fill(null),
+      win: false
+    }
+  }
+  checkWin() {
+    if (
+      this.state.pole3[0] === 3 &&
+      this.state.pole3[1] === 2 &&
+      this.state.pole3[2] === 1
+    ) {
+      //win do win stuff
+      console.log("win, do win stuff")
+    }
+  }
+  renderPoleGrid(myPole){
+    return(
+      //oh gosh what am I doing
+      )
+    )
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="container">
+        <h1>I will make this happen</h1>
+        <h1>Tower of Hanoi</h1>
+        <button>Reset</button>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
